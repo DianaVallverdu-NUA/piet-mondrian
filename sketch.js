8ebf7fe1d4ba3f9f0501983db3183ee46128d3ab
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-01-30 13:51:42 +0000

    add colored grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
 function setup() {
-    createCanvas(400, 400);
-}
-  
-function draw() {
+
+    //size of each square in pixels
+    const squareSize = 50;
+    //number of cells of the grid
+    const cellNumber = 10;
+    // canvas size in pixels
+    const canvasSize = squareSize * cellNumber;
+
+    createCanvas(canvasSize, canvasSize);
     background(220);
+
+    for(let row = 0; row < cellNumber; row++) {
+        for(let column = 0; column < cellNumber; column++) {
+            const red = random(255);
+            const green = random(255);
+            const blue = random(255);
+
+            const myColor = color(red, green, blue);
+
+            // use fill to fill all the square with one color
+            fill(myColor)
+
+            //calculate horizontal and vertical position in pixels
+            const x = row * squareSize;
+            const y = column * squareSize;
+
+            //calculate width and height
+            const width = squareSize;
+            const height = squareSize;
+            
+            // square(x, y, s) 
+            // x = position of the left of the square
+            // y = position of the top of the square
+            // s = size of the side of the square
+            // square(x, y, squareSize);
+            square(x, y, width)
+        }
+    }
 }
